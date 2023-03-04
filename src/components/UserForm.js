bb1d54a79619a11488b9e612760a92e91a10e258
AF Tamim <itamim345@gmail.com>
2023-03-05 04:19:26 +0530

    Modification in CSS & other components

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -7,7 +7,15 @@ export default function UserForm() {
     const {inputValues, handleChangeInput, setForm} = useUserform({
         fname: "",
         mname: "",
-        lname: ""
+        lname: "",
+        email: "",
+        mobile: "",
+        phone: "",
+        add1: "",
+        add2: "",
+        city: "",
+        country: "India"
+
     });
     const navigate = useNavigate()
     const {id} = useParams();
@@ -28,108 +36,148 @@ export default function UserForm() {
         }
     }, [id]);
   return (
-    <div className="form-container">
+    <div className="main-form-container">
       <h2>{id ? "Edit User" : "Add User"}</h2>
       <form onSubmit={handleSubmit}>
         <div className="name-container">
-          <label htmlFor="fname">First Name</label>
-          <input
-            type="text"
-            name="fname"
-            id="fname"
-            value={inputValues.fname}
-            onChange={handleChangeInput}
-            required
-          />
+          <div className="form-group">
+            <label htmlFor="fname">First Name</label>
+            <input
+              type="text"
+              name="fname"
+              id="fname"
+              value={inputValues.fname}
+              onChange={handleChangeInput}
+              required
+            />
+          </div>
 
-          <label htmlFor="mname">Middle Name</label>
-          <input
-            type="text"
-            name="mname"
-            id="mname"
-            value={inputValues.mname}
-            onChange={handleChangeInput}
-          />
+          <div className="form-group">
+            <label htmlFor="mname">Middle Name</label>
+            <input
+              type="text"
+              name="mname"
+              id="mname"
+              value={inputValues.mname}
+              onChange={handleChangeInput}
+            />
+          </div>
 
-          <label htmlFor="lname">Last Name</label>
-          <input
-            type="text"
-            name="lname"
-            id="lname"
-            value={inputValues.lname}
-            onChange={handleChangeInput}
-          />
+          <div className="form-group">
+            <label htmlFor="lname">Last Name</label>
+            <input
+              type="text"
+              name="lname"
+              id="lname"
+              value={inputValues.lname}
+              onChange={handleChangeInput}
+            />
+          </div>
         </div>
 
         <div className="contact-container">
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            value={inputValues.email}
-            onChange={handleChangeInput}
-          />
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={inputValues.email}
+              onChange={handleChangeInput}
+            />
+          </div>
 
-          <label htmlFor="mobile">Mobile</label>
-          <input
-            type="number"
-            name="mobile"
-            id="mobile"
-            value={inputValues.mobile}
-            onChange={handleChangeInput}
-          />
+          <div className="form-group">
+            <label htmlFor="mobile">Mobile</label>
+            <input
+              type="number"
+              name="mobile"
+              id="mobile"
+              value={inputValues.mobile}
+              onChange={handleChangeInput}
+            />
+          </div>
 
-          <label htmlFor="phone">Phone</label>
-          <input
-            type="number"
-            name="phone"
-            id="phone"
-            value={inputValues.phone}
-            onChange={handleChangeInput}
-          />
+          <div className="form-group">
+            <label htmlFor="phone">Phone</label>
+            <input
+              type="number"
+              name="phone"
+              id="phone"
+              value={inputValues.phone}
+              onChange={handleChangeInput}
+            />
+          </div>
         </div>
 
         <div className="address-container">
-          <label htmlFor="add1">Address 1</label>
-          <input
-            type="text"
-            name="add1"
-            id="add1"
-            value={inputValues.add1}
-            onChange={handleChangeInput}
-          />
+          <div className="form-group">
+            <label htmlFor="add1">Address 1</label>
+            <input
+              type="text"
+              name="add1"
+              id="add1"
+              value={inputValues.add1}
+              onChange={handleChangeInput}
+            />
+          </div>
 
-          <label htmlFor="add2">Address 2</label>
-          <input
-            type="text"
-            name="add2"
-            id="add2"
-            value={inputValues.add2}
-            onChange={handleChangeInput}
-          />
+          <div className="form-group">
+            <label htmlFor="add2">Address 2</label>
+            <input
+              type="text"
+              name="add2"
+              id="add2"
+              value={inputValues.add2}
+              onChange={handleChangeInput}
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="city">City</label>
+            <input
+              type="text"
+              name="city"
+              id="city"
+              value={inputValues.city}
+              onChange={handleChangeInput}
+            />
+          </div>
         </div>
 
         <div className="address-container-2">
-          <label htmlFor="city">City</label>
-          <input
-            type="text"
-            name="city"
-            id="city"
-            value={inputValues.city}
-            onChange={handleChangeInput}
-          />
-
-          <label htmlFor="state">State</label>
-          <input
-            type="text"
-            name="state"
-            id="state"
-            value={inputValues.state}
-            onChange={handleChangeInput}
-          />
+          <div className="form-group">
+            <label htmlFor="state">State</label>
+            <input
+              type="text"
+              name="state"
+              id="state"
+              value={inputValues.state}
+              onChange={handleChangeInput}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="country">Country</label>
+            <select
+              name="country"
+              value={inputValues.country}
+              onChange={handleChangeInput}
+            >
+              <option>India</option>
+              <option>Srilanka</option>
+              <option>Bangladesh</option>
+              <option>Pakistan</option>
+              <option>China</option>
+              <option>Nepal</option>
+              <option>Bhootan</option>
+            </select>
+          </div>
         </div>
-        <input type="submit" value={id ? "Edit User" : "Add User"} />
+        <input
+          className="submit-btn"
+          type="submit"
+          value={id ? "Edit User" : "Add User"}
+        />
       </form>
     </div>
   );
--- src/components/UserView.js
+++ src/components/UserView.js
@@ -1,36 +1,79 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-import { deleteUser, getUserById } from '../localStorage/localStorage';
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { deleteUser, getUserById } from "../localStorage/localStorage";
 
 export default function UserView() {
-    const [userdetail, setUserDetail] = useState({})
-    const navigate = useNavigate()
-    const {id} = useParams();
-    useEffect(() => {
-      setUserDetail(getUserById(id))
-    }, []);
+  const [userdetail, setUserDetail] = useState({});
+  const navigate = useNavigate();
+  const { id } = useParams();
+  useEffect(() => {
+    setUserDetail(getUserById(id));
+  }, []);
 
-    const handleDelete = () => {
-      deleteUser(id);
-      navigate("/")
-    };
+  const handleDelete = () => {
+    deleteUser(id);
+    navigate("/");
+  };
   return (
     <div>
-      <div>
-        <h1 onClick={() => navigate("/")}>Home</h1>
+      <div className="user-header">
+        <img
+          onClick={() => navigate("/")}
+          src="https://lh3.googleusercontent.com/a-/ACB-R5QSSQdO8vQF7KYax_52Z1cN6DcE7WGXe-IONXRb=s80-p"
+          alt="logo"
+        />
         <h2>User Details</h2>
-        <div>
+        <div className="user-buttons">
           <button onClick={() => navigate(`/edit-user/${id}`)}>Edit</button>
           <button onClick={() => handleDelete()}>Delete</button>
         </div>
       </div>
-      <div>
-        <h2>
-          Name: {userdetail.fname} {userdetail.mname} {userdetail.lname}
-        </h2>
-        <h2>Name: {userdetail.email}</h2>
-        <h2>Name: {userdetail.phone}</h2>
-        <h2>Name: {userdetail.mobile}</h2>
+
+      <div className="user-main">
+        <div className="personal-info">
+          <h3>Personal Info</h3>
+          <hr />
+          <p>
+            <strong>Name:</strong>
+            {` ${userdetail.fname} ${userdetail.mname} ${userdetail.lname}`}
+          </p>
+          <p>
+            <strong>Email: </strong>
+            {userdetail.email}
+          </p>
+          <p>
+            <strong>Phone: </strong>
+            {userdetail.phone}
+          </p>
+          <p>
+            <strong>Mobile: </strong>
+            {userdetail.mobile}
+          </p>
+        </div>
+        <div className="address-info">
+          <h3>Address Info</h3>
+          <hr />
+          <p>
+            <strong>Address 1: </strong>
+            {userdetail.add1}
+          </p>
+          <p>
+            <strong>Address 2: </strong>
+            {userdetail.add2}
+          </p>
+          <p>
+            <strong>City: </strong>
+            {userdetail.city}
+          </p>
+          <p>
+            <strong>State: </strong>
+            {userdetail.state}
+          </p>
+          <p>
+            <strong>Country: </strong>
+            {userdetail.country}
+          </p>
+        </div>
       </div>
     </div>
   );
